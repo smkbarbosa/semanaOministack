a81bdf886e6f3056bcae91c475f8ac958b273738
Samuel Barbosa <contato@smkbarbosa.eti.br>
2019-10-02 16:40:23 -0300

    add cors and path to access uploaded files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -17,7 +20,9 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW
 // req.params ( acessar route params para edição e delete)
 // req.body  (acessar corpo da requisição [criação, edição])
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.use(routes);
 
