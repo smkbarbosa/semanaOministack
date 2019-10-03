500f702dd876682736470cf0fc7ad01fdced9e9a
Samuel Barbosa <contato@smkbarbosa.eti.br>
2019-10-03 13:37:32 -0300

    Adding logo, labels, inputs, buttons at login page

--- mobile/pages/Login.js
+++ mobile/pages/Login.js
@@ -1,8 +1,87 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Platform, KeyboardAvoidingView, Image, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function Login(){
-    return <View>
-        <Text>Hello World!</Text>
+import logo from '../assets/logo.png';
+
+
+export default function Login() {
+    return (
+        <View style={styles.container}>
+            <Image source={logo} />
+
+            <KeyboardAvoidingView enabled={Platform.OS === 'ios'} behavior="padding" style={styles.form}>
+                <Text style={styles.label}>SEU EMAIL *</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Seu e-mail"
+                    placeholderTextColor="#999"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                />
+
+                <Text style={styles.label}>TECNOLOGIAS *</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Tecnologias de interesse"
+                    placeholderTextColor="#999"
+                    keyboardType="email-address"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>
+                        Encontrar spots
+                    </Text>
+                </TouchableOpacity>
+
+            </KeyboardAvoidingView>
         </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    form: {
+        alignSelf: 'stretch',
+        paddingHorizontal: 30,
+        marginTop: 30,
+    },
+
+    label: {
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8,
+    },
+
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20, 
+        borderRadius: 2
+    },
+
+    button: {
+        height: 42,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+    },
+
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+});
