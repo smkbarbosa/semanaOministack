db584b1ad3e75baed00e01f044d17c0cad814462
Samuel Barbosa <contato@smkbarbosa.eti.br>
2019-10-03 17:22:28 -0300

    List spots and create an reserve

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:3333',
+  baseURL: 'http://localhost:3333',
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': '*',
+  },
 });
 
 export default api;
+//exp://a5-u4f.smkbarbosa.mobile.exp.direct:80
